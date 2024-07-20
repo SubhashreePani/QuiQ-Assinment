47ddfbd06107d27ecc3c42c2e442d711720fa91d
SubhaShree <panisubhashree1652@gmail.com>
2024-07-20 18:54:05 +0530

    Fixing Creds

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyClSaECCTKdH4D8BzVmzGIt-2I7-vv42Us",
-  authDomain: "first-trial-4c77b.firebaseapp.com",
-  projectId: "first-trial-4c77b",
-  storageBucket: "first-trial-4c77b.appspot.com",
-  messagingSenderId: "464124915026",
-  appId: "1:464124915026:web:3c0683b2fe7c9777b09e8b",
+  apiKey: "Your_API_Key",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "Project_ID",
+  storageBucket: "Your_Bucket",
+  messagingSenderId: "Your_sender_id",
+  appId: "Your_app_id",
 };
 
 const app = initializeApp(firebaseConfig);
